765a4a9094f2d15b8f6771455f0e4859152541d5
AaronXue0 <her8153101@yahoo.com.tw>
2021-04-23 04:33:34 +0800

    Add prerender-spa

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: ["/", "/about", "/contact", "/games", "/sob"],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true,
+    },
+  },
 };
