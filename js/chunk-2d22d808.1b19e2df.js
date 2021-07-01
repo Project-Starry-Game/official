54f6aa8201707cba7facd92ce6d8e661690064b5
AaronXue0 <her8153101@yahoo.com.tw>
2021-07-01 09:46:40 +0800

    deploy

---
+++ js/chunk-2d22d808.1b19e2df.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d808"],{f888:function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v("Humand Resource page of starry")])},c=[],u={methods:{redirect:function(){}},mounted:function(){document.dispatchEvent(new Event("render-event")),this.redirect()}},d=u,o=t("2877"),i=Object(o["a"])(d,r,c,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d22d808.1b19e2df.js.map
